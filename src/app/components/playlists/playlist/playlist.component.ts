14df3a39a1d11fcaca68584ef6ba27195123c6a1
Nathaniel Truitt <ntruitt@Nathaniels-MacBook-Pro.local>
2022-10-25 13:37:05 -0500

    changing playlist component to onpush

--- src/app/components/playlists/playlist/playlist.component.ts
+++ src/app/components/playlists/playlist/playlist.component.ts
@@ -1,9 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlaylistComponent implements OnInit {
   @Input() tracks!: any[];
