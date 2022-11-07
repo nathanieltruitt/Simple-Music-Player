e8d7dd5f4fca491052ca8175a87306edd96388f8
Nathaniel Truitt <ntruitt@Nathaniels-MacBook-Pro.local>
2022-11-07 14:46:35 -0500

    changed to prod env file

--- src/app/services/data-access/spotify.service.ts
+++ src/app/services/data-access/spotify.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
