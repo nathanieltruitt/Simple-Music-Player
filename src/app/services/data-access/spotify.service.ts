eb212e416b9999e9a54c5394128e7d5b35ce1dd3
Nathaniel Truitt <ntruitt@Nathaniels-MacBook-Pro.local>
2022-11-14 12:06:54 -0600

    changed environment to test

--- src/app/services/data-access/spotify.service.ts
+++ src/app/services/data-access/spotify.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
